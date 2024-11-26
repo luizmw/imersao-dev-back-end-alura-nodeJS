8412e39b6c3a24ee7cefd7acef34a2211aeb11f1
luiz miguel <luizmiguelxy@gmail.com>
2024-11-26 19:10:43 -0300

    correção do arquivo services.sh

--- services.sh
+++ services.sh
@@ -1,4 +1,4 @@
 # scripts para subir o proj na google cloud
 gcloud services enable run.googleapis.com
-gcloud services enable cloudbuildapis.com
+gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
